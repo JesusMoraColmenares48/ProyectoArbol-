6eeca0f9c8484b817ae31c71d12e6ad831763d9c
Jesus Mora Colmenares <jesusmoracolmenares48@gmail.com>
2023-09-17 21:34:46 -0400

    Primer Com,it

---
+++ arbol.cpp
@@ -0,0 +1,83 @@
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+struct Nodo{
+	int dato;
+	Nodo *derecho;
+	Nodo *izquiero;
+};
+
+
+void mostrarArbol(Nodo *arbol, int contador){
+	
+	if(arbol == NULL){
+		return;
+	}else{
+		contador++;
+		mostrarArbol(arbol->derecho,contador);
+		for(int i = 0; i < contador; i++){
+			cout<< "   ";
+		}
+		cout<<arbol->dato<<endl;
+		contador++;
+		mostrarArbol(arbol->izquiero, contador);
+	}
+}
+
+Nodo *CrearNodo(int Dato){
+	Nodo *nodo=new Nodo();	
+	nodo->dato=Dato;
+	nodo->derecho=NULL;
+	nodo->izquiero=NULL;
+	
+	return nodo;
+}
+
+Nodo *ABB = NULL;
+
+void Insertar(Nodo *&arbol, int dato){
+	
+	if(arbol == NULL){
+		Nodo *nodo_nuevo = CrearNodo(dato);
+		arbol = nodo_nuevo;
+	}else{
+		int Raiz = arbol->dato;
+		if(dato < Raiz){
+			Insertar(arbol->izquiero,dato);
+		}else{
+			Insertar(arbol->derecho,dato);
+		}
+	}
+	
+
+}
+
+void Menu(){
+	int datoIngresado = 0;
+	int opcion = 0;
+	int contador = 0;
+	do{
+		cout<<"Ingresa una Opcion"<<endl;
+		cout<<"1- insertar nodo"<<endl;
+		cout<<"2-mostrar arbol"<<endl;
+		
+		cin>>opcion;
+		
+		if(opcion == 1){
+			cout<<"ingresa el numero , para el nuevo nodo que deseas ingresar: ";
+			cin>>datoIngresado;
+			Insertar(ABB,datoIngresado);
+		}else{
+			mostrarArbol(ABB,contador);
+		}
+	}while(opcion != 2);
+}
+
+
+
+int main(){
+	
+	Menu();
+}
+

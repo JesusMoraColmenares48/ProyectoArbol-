c701f692a65d2310891b089fa68b570d235d8404
JesusMoraColmenares48 <139276758+JesusMoraColmenares48@users.noreply.github.com>
2023-09-20 14:43:44 -0400

    Agregando funcionalidades calcular altura, balanceo, post orden , numero de hojas

--- arbol.h
+++ arbol.h
@@ -3,13 +3,56 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+
 struct Nodo{
 	string dato;
 	Nodo *derecho;
 	Nodo *izquiero;
+	int numHojas;
+	vector<string> DatoDeHojas;
+	bool Balanceado = false;
+
 };
+int numeroHojas=0;
+vector<string> Hojas;
+
+//metodo utilizado para poder determinar el numero de niveles y asi con el Metodo ArbolBalanceado determinar si el arbol esta balanceado o no
+int ContarNiveles(Nodo *arbol) {
+    if (arbol == NULL)
+        return 0;
+    return max(ContarNiveles(arbol->izquiero), ContarNiveles(arbol->derecho)) + 1;
+}
+
+//metodo utilizado para determinar la diferencia de niveles entre la rama izquierda y derecha y asi determinar si el arbol esta balanceado o no
+bool ArbolBlanceado(Nodo* arbol) {
+    if (arbol == NULL)
+        return true;
+
+    int AlturaRamaIzquierda = ContarNiveles(arbol->izquiero);
+    int AlturaRamaDerecha = ContarNiveles(arbol->derecho);
+
+    if (abs(AlturaRamaIzquierda - AlturaRamaDerecha) <= 1 && ArbolBlanceado(arbol->izquiero) && ArbolBlanceado(arbol->derecho))
+        return true;
+
+    return false;
+}
+
+//metodo utilizado pàra poder contar la altura de cada arbol , midiendo la latura de la rama izquierda y derecha y determinar cual es mayor y asi saber que altura tiene
+int calcularAltura(Nodo *arbol){
+	if(arbol==NULL){
+		return 0 ;
+	}else{
+		int alturaizquierda = calcularAltura(arbol->izquiero);
+		int alturaDerecha = calcularAltura(arbol->derecho);
+		
+		return max(alturaizquierda, alturaDerecha)+1;
+	}
+}
 
 
+
+//metodo utilizado para imprimir por pantalla los arboles mediante recursividad
 void mostrarArbol(Nodo *arbol, int contador){
 	
 	if(arbol == NULL){
@@ -20,12 +63,13 @@ void mostrarArbol(Nodo *arbol, int contador){
 		for(int i = 0; i < contador; i++){
 			cout<< "   ";
 		}
-		cout<<arbol->dato<<endl;
 		contador++;
+		cout<<arbol->dato<<endl;
 		mostrarArbol(arbol->izquiero, contador);
 	}
 }
 
+//metodo utilizado para inicializar los nodos de los arboles, los cuales contendran un string como dato , un hijo izquierdo y un hijo derecho
 Nodo *crearNodo(string Dato){
 	Nodo *nodo=new Nodo();	
 	nodo->dato=Dato;
@@ -35,8 +79,12 @@ Nodo *crearNodo(string Dato){
 	return nodo;
 }
 
+
+
 Nodo *ABB = NULL;
 
+
+//metodo para poder agregar nuevos nodos al arbol , determinando si deben ponerse a la izquierda o a la derecha
 void insertar(Nodo *&arbol, string dato){
 	
 	if(arbol == NULL){
@@ -56,25 +104,22 @@ void insertar(Nodo *&arbol, string dato){
 
 }
 
-// void menu(){
-// 	string datoIngresado = "";
-// 	int opcion = 0;
-// 	int contador = 0;
-// 	do{
-// 		cout<<"Ingresa una Opcion"<<endl;
-// 		cout<<"1- insertar nodo"<<endl;
-// 		cout<<"2- mostrar arbol"<<endl;
-// 		cout << "Input: ";
-// 		
-// 		cin>>opcion;
-// 		
-// 		if(opcion == 1){
-// 			cout<<"ingresa el numero , para el nuevo nodo que deseas ingresar: ";
-// 			cin>>datoIngresado;
-// 			insertar(ABB,datoIngresado);
-// 		}else{
-// 			mostrarArbol(ABB,contador);
-// 		}
-// 	}while(opcion != 2);
-// }
-//
+//metodo para hacer el recorrido post orden y determinar el numero de hojas de cada dato revisando si cada nodo tiene hijos o no
+void RecorerPostOrden(Nodo *arbol){
+	if(arbol == NULL){
+		return;
+	}
+	else{
+		RecorerPostOrden(arbol->izquiero);
+		RecorerPostOrden(arbol->derecho);
+		cout<<arbol->dato<<" - ";
+	
+		
+		if(arbol->izquiero == NULL && arbol->derecho == NULL){
+			Hojas.push_back(arbol->dato);			
+			numeroHojas++;
+		}		
+	}
+}
+
+
